99fff6b0f0a04cfebbca611b1e7db3f6baf93b2e
velvizhi1703 <A00325399@student.tus.ie>
2025-01-22 12:58:09 +0000

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 /**
  * Unit test for simple App.
@@ -14,7 +14,7 @@ public class AppTest
     public void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
-        assertTrue(app1.getMessage(), app2.getMessage());
+        assertEquals(app1.getMessage(), app2.getMessage());
     }
 
     @Test
